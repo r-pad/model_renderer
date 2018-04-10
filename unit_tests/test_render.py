eb0b87916dc9288858b467695da0fa642e9dfd34
Brian Okorn <bokorn@andrew.cmu.edu>
2018-04-10 01:47:01 +0000

    Initial Commit

---
+++ unit_tests/test_render.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  9 21:14:47 2018
+
+@author: bokorn
+"""
+from model_renderer.pose_renderer import renderView
+from model_renderer.transformations import random_quaternion
+
+def testRenderView():
+    camera_dist = 2
+    pose = random_quaternion()
+    filenames = ['plane.png']
+    model_file = 'plane/model_normalized.obj'
+
+    pose_quats = [pose]
+    renderView(model_file, pose_quats, camera_dist, filenames=filenames, 
+               standard_lighting=True, debug_mode=False)
+    
+if __name__ == '__main__':
+    testRenderView()
+    
